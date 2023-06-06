ab35512a99c35a0d19058709010a619302f3f2b2
lomaferreira <lomasantos666@gmail.com>
2023-06-05 21:27:26 -0300

    add point count

--- init.py
+++ init.py
@@ -3,16 +3,26 @@ import pygame  # importa a biblioteca
 from pygame.locals import *
 # importa do modulo sys a função exit(fecha a janela do jogo)
 from sys import exit
+from random import randint #importa a funçao randint(sortea valores)
 
 pygame.init()  # inicializa todas as funções e variaveis
 
 
 largura = 640
 altura = 480
-x=largura/2 #controlam o movimento dos objetos(ta no centro(n exatamente))
-y=altura/2
+x = largura/2  # controlam o movimento dos objetos(ta no centro(n exatamente))
+y = altura/2
 
-relogio= pygame.time.Clock() #velocidade do movimento
+x_azul=randint(40,600) #posições de escolha pra x, desconsiderando a altura do retangulo pra n sair da tela
+y_azul=randint(50,430)
+
+
+pontos=0
+#pygame.font.get_fonts() ver as fontes disponiveis
+fonte=pygame.font.SysFont('arial',40,True,True)
+
+
+relogio = pygame.time.Clock()  # velocidade do movimento
 
 # cria um objeto que vai ser a tela largura=640 e h=480
 tela = pygame.display.set_mode((largura, altura))
@@ -20,9 +30,14 @@ pygame.display.set_caption("Game")  # muda o nome da janela
 
 # loop infinito principal do jogo(atualização do jogo)
 while True:
-    tela.fill((0,0,0))#preenche a tela da cor preta a cada interação(n ver a prolongação do movimento )
-    relogio.tick(50)
-   
+    # preenche a tela da cor preta a cada interação(n ver a prolongação do movimento )
+    tela.fill((0, 0, 0))
+    relogio.tick(30)
+
+    mensagem=f"Pontos:{pontos}"
+
+    texto_formatado=fonte.render(mensagem,True,(255,255,255)) #uni a msg e a fonte(mensagem, false=maisPixelado)
+ 
     # detecta se algum evento ocorreu
     for event in pygame.event.get():
         # testa se o evento fechar a janela do jogo ocorreu
@@ -30,40 +45,62 @@ while True:
             pygame.quit()
             exit()
 
-    if pygame.key.get_pressed()[K_a]:# Se teclar ou precionar a tecla A
-         x-=20
+    if pygame.key.get_pressed()[K_a]:  # Se teclar ou precionar a tecla A
+        x -= 20
     if pygame.key.get_pressed()[K_d]:
-        x+=20
+        x += 20
     if pygame.key.get_pressed()[K_w]:
-        y-=20
+        y -= 20
     if pygame.key.get_pressed()[K_s]:
-        y+=20             
+        y += 20
 
-    pygame.draw.rect(tela,(255,0,0),(x,y,40,50))#desenha objeto na tela(superfice, cor, posição(x,y,width,height))
+       
+
+    # desenha objeto na tela(superfice, cor, posição(x,y,width,height))
+    ret_vermelho=pygame.draw.rect(tela, (255, 0, 0), (x, y, 40, 50))
+    ret_azul=pygame.draw.rect(tela,(0,0,225),(x_azul,y_azul,40,50))
     
-   
-    
-    
-    
+    #colliderect método do retangulo de colizão
+    if ret_vermelho.colliderect(ret_azul):
+        x_azul=randint(40,600)
+        y_azul=randint(50,430)
+        pontos+=1
+
+    tela.blit(texto_formatado,(450,440))#rederiza a msg na tela na seguinte posição
+
     # a cada interação do loop principal atualiza o jogo, para nao travar
     pygame.display.update()
 
-
-
-
-
-
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
     '''
     pygame.draw.circle(tela,(0,98,55),(300,260),40)# 40 é o raio
     pygame.draw.line(tela,(255,255,0),(390,10),(90,600),10)# coordenada inicio e fim da linha e espersura da linha
     '''
-    
+
     '''
     if(y>=altura):  #não sumir da tela o objeto
         y=0
     y+=1
     '''
-
 
     '''
         #controla movimentos(dentro do for event) 
@@ -76,5 +113,4 @@ while True:
                y-=20
            if event.key==K_s: #tecla S
                y+=20         
-        '''  
-    
+        '''
